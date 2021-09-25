ee0952f80b2d533f3924606872ed2d9c66699716
Sushant <sushantkumar6700@gmail.com>
2021-09-25 20:02:53 +0530

    renaming of folder

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import classes from './Home.css'
-import RightPanel from './RightPanel/RightPanel'
+import RightPanel from './RightSection/RightPanel'
 import LeftSection from './LeftSection/LeftSection';
 function Home() {
     return (
