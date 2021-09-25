5fb5aba5c37dcf87812992b91cdf6030a7b737a2
Sushant <sushantkumar6700@gmail.com>
2021-09-25 17:15:00 +0530

    Right panel added to home page

--- src/containers/Home/RightPanel/RightPanel.js
+++ src/containers/Home/RightPanel/RightPanel.js
@@ -1,14 +1,19 @@
 import React , {Component} from "react";
 
-
-
 //This Component will be used to render right Panel of home page.
+import Profile from "../../../components/RightPanel/Profile/Profile";
+import TopNews from "../../../components/RightPanel/TopNews/TopNews";
+import classes from './RightPanel.css';
+
 
 class RightPanel extends Component {
 
     render(){
         return (
-            <p>Hello world!!</p>
+            <div className={classes.RightPanel}>
+                <Profile />
+                <TopNews />
+            </div>
         );
     }
 }
