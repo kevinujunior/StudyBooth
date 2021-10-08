2070979ed965c47e06630f9511ac677c439f8ded
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2021-10-08 23:27:03 +0530

    Login Signup Pages classes update

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react';
-import '../auth/css/Login-Signup.css'
+import classes from '../auth/css/Login-Signup.css'
 export class Signup extends Component {
 
   render() {
     return(
      
-    <div class = "signup-main">
-       <section class="signup">
-            <div class="container">
-                <div class="signup-content">
-                    <div class="signup-form">
-                        <h2 class="form-title">Sign up</h2>
-                        <form method="POST" class="register-form" id="register-form">
-                            <div class="form-group">
+    <div class = {classes.signupMain}>
+       <section class={classes.signup}>
+            <div class={classes.container}>
+                <div class= {classes.signupContent}>
+                    <div class={classes.signupForm}>
+                        <h2 class={classes.formTitle}>Sign up</h2>
+                        <form method="POST" class={classes.registerForm} id={classes.registerForm}>
+                            <div class={classes.formGroup}>
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
                                 <input type="text" name="name" id="name" placeholder="Your Name"/>
                             </div>
-                            <div class="form-group">
+                            <div class={classes.formGroup}>
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
                                 <input type="email" name="email" id="email" placeholder="Your Email"/>
                             </div>
-                            <div class="form-group">
+                            <div class={classes.formGroup}>
                                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
                                 <input type="password" name="pass" id="pass" placeholder="Password"/>
                             </div>
-                            <div class="form-group">
+                            <div class={classes.formGroup}>
                                 <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
                                 <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
                             </div>
-                            <div class="form-group">
-                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
-                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
+                            <div class={classes.formGroup}>
+                                <input type="checkbox" name="remember-me" id="remember-me" class={classes.agreeTerm} />
+                            <label for="remember-me" class={classes.labelAgreeTerm}><span><span></span></span>I agree all statements in  <a href="#" class={classes.termService}>Terms of service</a></label>
                             </div>
-                            <div class="form-group form-button">
-                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                            <div className = {[classes.formGroup, classes.formButton].join(" ")}>
+                                <input type="submit" name="signup" id={classes.signup} class={classes.formSubmit} value="Register"/>
                             </div>
                         </form>
 
 
                     </div>
-                    <div class="signup-image">
+                    <div class={classes.signupImage}>
                         <figure>
                           <img src ="images/signup-image.jpg" alt=""/>
                         </figure>
-                        <a href="/login" class="signup-image-link">I am already member</a>
+                        <a href="/login" class={classes.signupImageLink}>I am already member</a>
                     </div>
                 </div>
             </div>
