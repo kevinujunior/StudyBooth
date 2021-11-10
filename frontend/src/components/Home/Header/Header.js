e771b3ab5772df994c6e9193c54495563f7abeeb
DIvyanshu Bose <56763659+theventurer@users.noreply.github.com>
2021-11-10 23:02:28 +0530

    search + follow request

--- frontend/src/components/Home/Header/Header.js
+++ frontend/src/components/Home/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import * as actionsTypes from '../../../store/actions/actionTypes';
 
@@ -6,16 +6,64 @@ import classes from './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import logo from '../../../assets/Study_Booth.png'
 import NotificationsNoneRoundedIcon from '@mui/icons-material/NotificationsNoneRounded';
-import { IconButton } from '@mui/material';
+import { Avatar, Button, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import ToggleButton from '../../UI/ToggleButton/ToggleButton';
 import AddIcon from '@mui/icons-material/Add';
+import axios from 'axios';
+import Spinner from '../../UI/Spinner/Spinner';
+
 
 function Header(props) {
+
+    
+    const [searchinput, setSearchInput] = useState("");
+    let [searchDropdown,setSearchDropdown] = useState({});
+    let [dropdownMenu,setDropdownMenu] = useState();
+    const [followData,setFollowData] = useState({});
     let headerClasses = [classes.Header];
     if(props.theme === 'dark'){
         headerClasses.push(classes.Dark);
     }
+     
+    const fetchSearch = () => {
+        axios.get(`http://localhost:8000/users/userview/?user=${searchinput}`)
+        .then(response =>{
+            const search = response.data
+            console.log("searching..")
+            setSearchDropdown(search)
+            // console.log(search)
+            console.log(Object.entries(searchDropdown)) 
+            // if(Object.entries(searchDropdown) > 0)    
+            const postFollow = (data) =>{
+                axios.post(`http://localhost:8000/users/followingview/`, data)
+                .then(response =>{
+                    console.log(response);
+                })
+                .catch(err => {
+                    console.log(err)
+                });
+            }
+
+            const number_of_users = Object.keys(search).length;
+            setDropdownMenu([...Array(number_of_users)].map((x, i) => {
+                return  <li value={search[i]["username"]}>
+                        <Avatar alt={search[i]["username"]} src={search[i]["userPic"]} />
+                        <p>{search[i]["fullName"]}</p>
+                        <Button 
+                        variant="contained" 
+                        size="small"
+                        onClick={postFollow({
+                            currUser: 1,
+                            followingUser: search[i]["id"]})}>Follow</Button>
+                        </li> 
+            }))
+             })
+        .catch(err => {
+            console.log(err)
+        });      
+    } 
+
 
     return (
         <div className={headerClasses.join(" ")}>
@@ -30,9 +78,21 @@ function Header(props) {
                 </div>
                 <div className={classes.SearchAndCreate}>
                     <div className={classes.Input}>
-                        <input type="text" class={classes.inputSearch} placeholder="Type to Search..."></input>
-                        <button class={classes.btnSearch}>{<SearchIcon/>}</button>
+                        <input 
+                        type="text" 
+                        class={classes.inputSearch}     
+                        value={searchinput}
+                        onChange={e => setSearchInput(e.target.value)}
+                        placeholder="Type to Search...">
+                        </input>
+                        <button class={classes.btnSearch} onClick={fetchSearch}>{<SearchIcon/>}</button>
                     </div>
+                    
+                <div className={classes.searchDropdown}>
+                    <ul class={classes.searchDropdownMenu} aria-label="submenu">
+                        {dropdownMenu}
+                    </ul>
+                </div>
                     <div>
                         <button  className={classes.CreatePostButton1} onClick={props.onCreateFeedClick}>
                             <p>Create Post</p>
@@ -40,6 +100,7 @@ function Header(props) {
                         </button>
                     </div>
                 </div>   
+                
                 <div className={classes.Buttons}>
                     <div className={classes.ToggleBtn}>
                         <ToggleButton theme={props.theme} onClick = {() => props.onChangeTheme(props.theme)} />
@@ -53,7 +114,9 @@ function Header(props) {
                         <AddIcon style={{color:'white'}} />
                     </button>
                 </div>             
+                
             </div>
+            
         </div>
     )
 }
