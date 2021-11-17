ea49520905aa7b2e23178d2e7ef2b5f4b1e99dce
Rajk1012 <86977745+Rajk1012@users.noreply.github.com>
2021-11-17 18:21:20 +0530

    chatIn

--- frontend/src/containers/Home/Chat/chatIn.js
+++ frontend/src/containers/Home/Chat/chatIn.js
@@ -1,4 +1,4 @@
-import { Avatar, IconButton } from "@mui/material";
+import { Avatar, IconButton, MenuItem, Menu } from "@mui/material";
 import { AttachFile, MoreVert, SearchOutlined } from "@mui/icons-material";
 import React, { useState, useEffect, createRef } from "react";
 import { InsertEmoticonOutlined, Mic, Send } from "@mui/icons-material";
@@ -20,17 +20,28 @@ function ChatIn() {
     console.log("You typed >>>", input);
   };
 
-  const pickEmoji = (e, {emoji}) =>{
+  const pickEmoji = (e, { emoji }) => {
     const Input = input + emoji;
     setInput(Input);
   };
-
+  
   const handleShowEmojis = () => {
     setShowEmojis(!showEmojis);
   };
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
   return (
+
     <div className={classes.chat}>
+
       <div className={classes.chat_header}>
         <Avatar sec={"https://avatar.dicebear.com/api/human/${loading}.svg"} />
         <div className={classes.chat_headerInfo}>
@@ -43,12 +54,30 @@ function ChatIn() {
           </IconButton>
           <IconButton>
             <AttachFile />
+
           </IconButton>
-          <IconButton>
+          <IconButton
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}>
             <MoreVert />
+            <Menu
+              keepMounted
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              open={Boolean(anchorEl)} className={classes.menu}
+            >
+              <MenuItem onClick={handleClose}>Group info</MenuItem>
+              <MenuItem onClick={handleClose}>Media</MenuItem>
+              <MenuItem onClick={handleClose}>Search</MenuItem>
+              <MenuItem onClick={handleClose}>mute notification</MenuItem>
+              <MenuItem onClick={handleClose}>Exit group</MenuItem>
+              <MenuItem onClick={handleClose}>more</MenuItem>
+            </Menu>
           </IconButton>
         </div>{" "}
       </div>
+
       <div className={classes.chat_body}>
         {/* <p className={`classes.chat_message ${true && 
                 "chat_receiver"}`}> */}
@@ -57,8 +86,8 @@ function ChatIn() {
           {/* <span className={classes.chat_name}>rajkumar</span> */}
           Hey Guys
           <span className={classes.chat_timestamp}>5:25pm</span>
-        </p>
-        {!showEmojis && <Emoji pickEmoji={pickEmoji}/>}
+          </p>
+          {!showEmojis && <Emoji pickEmoji={pickEmoji} />}
       </div>
 
       <div className={classes.chat_footer}>
@@ -67,7 +96,7 @@ function ChatIn() {
           style={styles.button}
         />
         <form>
-        
+
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
