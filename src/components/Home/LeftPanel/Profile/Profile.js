7889cee33fdfab6f9816feb6e6e0f2b09b768827
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2021-11-20 19:11:11 +0530

    Revert "default profiles"
    This reverts commit a9cbfa4c74ec418580f4fb291a3b3fee73fb7ee7.

--- src/components/Home/LeftPanel/Profile/Profile.js
+++ src/components/Home/LeftPanel/Profile/Profile.js
@@ -8,13 +8,12 @@ function Profile(props){
     let history = useHistory();
     
     return (
-        
         <div className={classes.Profile}>
             <div className = {classes.Info}>
                 <div className={classes.InfoBox}>
                     {/* photo and name*/}
                     <div onClick={() => history.push('/profile')} style={{'cursor':'pointer'}}>
-                        <img src={props.userData.userPic ? props.userData.userPic : "https://yourwikis.com/wp-content/uploads/2020/01/mark-zuck-img.jpg"} />
+                        <img src={props.userData ? props.userData.userPic : "https://yourwikis.com/wp-content/uploads/2020/01/mark-zuck-img.jpg"} />
                     </div>
                     <p>{props.userData ? props.userData.fullName : 'Alien'}</p>
                     <p style={{fontSize:"13px"}}>Lives on mars.</p>
--- src/components/Profile/MainSection/UserPosts.js
+++ src/components/Profile/MainSection/UserPosts.js
@@ -10,8 +10,7 @@ class Posts extends Component{
         console.log(this.props.posts)
         if(this.props.posts){
             posts = [...Array(this.props.posts.length)].map((_,i) => {
-                console.log(this.props.posts[i].postFile)
-                return <img src={this.props.posts[i].postFile?this.props.posts[i].postFile:"https://www.wpbeginner.com/wp-content/uploads/2013/04/wp404error.jpg"} onError={(e)=>{e.target.onerror = null; e.target.src="https://www.wpbeginner.com/wp-content/uploads/2013/04/wp404error.jpg"}} className={classes.ImgBox} key={this.props.posts[i].id} alt=""/>;
+                return <img src={this.props.posts[i].postFile} className={classes.ImgBox} key={this.props.posts[i].id} alt=""/>;
             })
         }
         return(
