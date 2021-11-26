f9e7b52bc20ac6f141718ac6acace3482849a15b
DIvyanshu Bose <56763659+theventurer@users.noreply.github.com>
2021-11-26 22:20:52 +0530

    animation

--- src/components/Home/MainSection/Feed/Feed.js
+++ src/components/Home/MainSection/Feed/Feed.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import Post from './Post/Post';
 import Spinner from '../../../UI/Spinner/Spinner'
-
 import {connect } from 'react-redux';
 import * as actions from '../../../../store/actions/index';
+import { AnimatePresence, motion } from 'framer-motion';
+const blackBox = {
+    initial: {
+      height: "100vh",
+      bottom: 0,
+    },
+    animate: {
+      height: 0,    
+    },
+  };
+  
+const InitialTransition = () => {
+return (
+    <div className="absolute inset-0 flex items-center justify-center">
+    <motion.div
+        className="relative z-50 w-full bg-black"
+        initial="initial"
+        animate="animate"
+        variants={blackBox}
+    />      
+    </div>
+);
+}
 
 class Feed extends Component {
     
@@ -15,11 +37,49 @@ class Feed extends Component {
     render () {
         
         let posts  = <Spinner/>;
-        
+        const content = {
+            animate: {
+              transition: { staggerChildren: 0.1, delayChildren: 2.8 },
+            },
+          };
+          
+          const title = {
+            initial: { y: -20, opacity: 0 },
+            animate: {
+              y: 0,
+              opacity: 1,
+              transition: {
+                duration: 1.5,
+                ease: [0.6, -0.05, 0.01, 0.99],
+              },
+            },
+          };
+          
+          const products = {
+            initial: { y: -20, opacity: 0 },
+            animate: {
+              y: 0,
+              opacity: 1,
+              transition: {
+                duration: 0.7,
+                ease: [0.6, -0.05, 0.01, 0.99],
+              },
+            },
+          };
+
         if(this.props.posts != null && this.props.posts.length > 0){
             const number_of_posts = Object.keys(this.props.posts).length;
             posts = [...Array(number_of_posts)].map((x, i) => {
-                return <Post
+                return <AnimatePresence>
+                <motion.div
+                animate={{
+                  scale: [0.1, 0.4, 0.7, 1.1, 1],
+                  borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+                  
+                }}
+                // transition={{ ease: "easeOut", duration: 2 }}
+              >
+                <Post
                     key={this.props.posts[i].id}
                     id  = {this.props.posts[i].id}
                     userId = {this.props.posts[i].user}
@@ -34,7 +94,9 @@ class Feed extends Component {
                     commentCount={this.props.posts[i].commentCount}
                     isLikedByuser = {this.props.posts[i].isLiked}
                     likeId = {this.props.posts[i].likeId}
-                /> 
+                />
+                </motion.div> 
+                </AnimatePresence>
             })
         }
 
@@ -42,7 +104,25 @@ class Feed extends Component {
 
         return (
             <div>
-                {posts}
+                {/* <AnimationComponent /> */}
+                {/* {posts} */}
+                <motion.section exit={{ opacity: 0 }}>
+                <InitialTransition/>
+                <motion.div
+                    initial="initial"
+                    animate="animate"
+                    variants={content}
+                    className="space-y-12"
+                >
+                    <motion.div variants={title} className="text-6xl font-black text-center">
+                    {posts}
+                    </motion.div>
+
+                    <motion.section variants={products} className="text-gray-700 body-font">
+                    
+                    </motion.section>
+                </motion.div>
+                </motion.section>
             </div>
         );
     }
